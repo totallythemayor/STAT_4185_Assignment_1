c110f0ea1ac6be928e914b6d6f3a03897c29fd31
Mayurapriyan Somalinga <138075166+totallythemayor@users.noreply.github.com>
2023-09-12 00:58:52 -0400

    Decrypted Message One

--- decrypt_message_one.py
+++ decrypt_message_one.py
@@ -41,3 +41,17 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write code below
+
+key_list = list(cipher.keys())
+val_list = list(cipher.values())
+
+def decrypt_message_one(message):
+    decrypted_message = ""
+    for letter in message:
+        position = val_list.index(letter)
+        decrypted_message += key_list[position]
+    return decrypted_message
+    
+
+print(decrypt_message_one(encrypted_message))
+
