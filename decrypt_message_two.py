3e4f5572e650feb008964a9917ff9e11f75f9ba7
Mayurapriyan Somalinga <138075166+totallythemayor@users.noreply.github.com>
2023-09-12 01:23:45 -0400

    Decrpyted Message Two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,17 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+def decrypt_message_two(message):
+    result = ""
+    mess = list(message)
+    mess_rev = mess[::-1]
+    for i in range(len(mess_rev)):
+        if i % 2 == 0:
+            result += mess[i]
+        else:
+            result += mess_rev[i]
+    return result
+
+
+print(decrypt_message_two(encrypted_message))
